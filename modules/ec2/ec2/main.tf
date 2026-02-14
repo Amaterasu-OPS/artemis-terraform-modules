68ae2793633404303ffe47151b2e665bff6773d8
Miguel Colombo <miguelhunter95@gmail.com>
2026-02-13 23:00:52 -0300

    fix: fix script path

--- modules/ec2/ec2/main.tf
+++ modules/ec2/ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "this" {
   depends_on                  = [var.dependencies]
   key_name                    = var.key_name
   associate_public_ip_address = var.public_ip
-  user_data                   = "${file(path.module, "/scripts/user_data.sh")}${var.user_data != "" ? var.user_data : ""}"
+  user_data                   = "${file("../../../scripts/user_data.sh")}${var.user_data != "" ? var.user_data : ""}"
   iam_instance_profile        = var.iam_instance_profile
 
   metadata_options {
