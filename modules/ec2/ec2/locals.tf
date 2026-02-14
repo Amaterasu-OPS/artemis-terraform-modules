f660dbdb212c74d1b39e8813729793ab4978bd92
Miguel Colombo <miguelhunter95@gmail.com>
2026-02-14 18:15:00 -0300

    fix: fix locals

--- modules/ec2/ec2/locals.tf
+++ modules/ec2/ec2/locals.tf
@@ -3,10 +3,10 @@ locals {
   cw_log_group_name  = "/ec2/${var.cluster_name}/${var.cluster_type}/${var.cluster_instance_type}"
 
   script = templatefile("${path.module}/ec2_base.sh", {
-    ssm_parameter_name = "ssm:${locals.ssm_parameter_name}"
+    ssm_parameter_name = "ssm:${local.ssm_parameter_name}"
   })
 
   cw_config = templatefile("${path.module}/cw_agent_config.json", {
-    log_group_name = locals.cw_log_group_name
+    log_group_name = local.cw_log_group_name
   })
 }
