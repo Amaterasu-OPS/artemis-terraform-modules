d7ac021c9d3e03b1c3e77efac0ef664a49ef05c7
Miguel Colombo <miguelhunter95@gmail.com>
2026-02-14 12:38:44 -0300

    fix: remove unused vars

--- modules/iam/roles/vars.tf
+++ modules/iam/roles/vars.tf
@@ -21,13 +21,3 @@ variable "tags" {
   default     = {}
   description = "A map of tags to assign to the resource."
 }
-variable "policy" {
-  type        = string
-  nullable    = false
-  description = "The policy to attach to the role."
-}
-variable "ec2_role_name" {
-  type        = string
-  nullable    = false
-  description = "The name of the EC2 role to attach the policy to."
-}
